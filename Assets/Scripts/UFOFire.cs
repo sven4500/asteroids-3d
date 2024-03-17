5e409fe118701b79922f5097b39e10f68f26d83f
Ivars <sven4500@mail.ru>
2024-03-17 18:14:36 +0300

    Fir laser beam rotation

--- Assets/Scripts/UFOFire.cs
+++ Assets/Scripts/UFOFire.cs
@@ -7,8 +7,12 @@ public class UFOFire: MonoBehaviour {
 	void Update() {
         _time += Time.deltaTime;
 		if(_time > _fireRate && Input.GetKeyDown(KeyCode.Space) == true) {
+			Quaternion rot = Quaternion.identity;
+			rot.eulerAngles = new Vector3(90, 0, 0);
+			
             Vector3 pos = transform.position;
-            Instantiate(Resources.Load("LaserCharge"), pos, Quaternion.identity);
+			
+            Instantiate(Resources.Load("LaserCharge"), pos, rot);
             _time = 0.0F;
         }
 	}
